35e56825966d77f0cb4ab63bc45b6674c487e30f
mgd666 <1291788563@qq.com>
2023-02-17 20:24:47 +0800

    first commit

--- src/main/java/com/example/forum/ForumServletInitializer.java
+++ src/main/java/com/example/forum/ForumServletInitializer.java
@@ -1,2 +1,15 @@
-package com.example.forum;public class ForumServletInitializer {
+package com.example.forum;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * 线上部署上线后的程序入口
+ */
+public class ForumServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ForumApplication.class);
+    }
 }
---
+++ src/main/java/com/example/forum/aspect/ServiceLogAspect.java
@@ -0,0 +1,4 @@
+package com.example.forum.aspect;
+
+public class ServiceLogAspect {
+}
